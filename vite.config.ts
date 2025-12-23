f0c6060f947f56823cf75bb9a05dcab1275352ad
Ledge1lAnk <workoutman1158@gmail.com>
2025-12-23 05:34:10 +0100

    Fix v2.7: Reliable API_KEY injection

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()],
     define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+      'process.env.API_KEY': JSON.stringify(process.env.API_KEY || env.API_KEY),
     },
     resolve: {
       alias: {
