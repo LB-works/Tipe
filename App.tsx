2fb377cfc1a05803cfe0425027fc8f87e0122a3a
Ledge1lAnk <workoutman1158@gmail.com>
2025-12-23 04:55:11 +0100

    Fix v2.5: Syntax cleanup and model switch

--- App.tsx
+++ App.tsx
@@ -153,7 +153,3 @@ const App: React.FC = () => {
         <p>© 2024 ThoughtRefine AI • Professional Thought Polishing • v2.5</p>
       </footer>
     </div>
-            );
-};
-
-            export default App;
